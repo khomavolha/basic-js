ac187ebb187e948f4f74b7676d5f789fceb360c8
Khoma Olya <52779547+khomavolha@users.noreply.github.com>
2020-03-17 20:51:00 +0300

    feat: add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,22 +1,32 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
+	arr: [],
+	getLength() {
+		return this.arr;
+	},
+	addLink(value) {
+		this.arr.push(value);
+		return this;
+	},
+	removeLink(position) {
+		if (typeof position !== "number" || position<=0 || position>this.arr.length){
+			this.arr = [];
+			throw Error();
+		}
+		this.arr.splice(position - 1, 1);
+		return this;
+	},
+	reverseChain() {
+
+		this.arr.reverse();
+		return this;
+	},
   finishChain() {
-    throw 'Not implemented';
+    let arr2 = this.arr.map(function(map){
+      return `( ${map} )`
+    }).join('~~');
+    this.arr=[];
+    return arr2;
+
     // remove line with error and write your code here
   }
 };
