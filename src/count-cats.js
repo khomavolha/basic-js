f437c620389a0fc4c18a74a3882a5637aa57ddf1
Khoma Olya <52779547+khomavolha@users.noreply.github.com>
2020-03-17 16:00:40 +0300

    feat: add solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+let arr = matrix.flat(Infinity);
+let arr2=[];
+for(let i = 0; i<arr.length; i++){
+if(arr[i]=='^^'){
+arr2.push(arr[i])
+}}
+return arr2.length;
 };
+
