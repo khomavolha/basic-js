999ed07cf37eb9c60ebaf1ac9d2f6b9fe4b7cc68
Khoma Olya <52779547+khomavolha@users.noreply.github.com>
2020-03-17 21:37:28 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+if (members!==undefined &&members!==null && typeof members ==='object' && members.length>0){
+let newArr = [];
+let result = [];
+for (let i in members){
+  if(typeof members[i] === 'string'){
+    members[i].trim();
+    newArr.push(members[i].trim()[0].toUpperCase());
+  }}
+return newArr.sort().join('')
+}else{return false}
+
 };
