795a53680ed2ebff1994ebbacf992c7a397cca57
Khoma Olya <52779547+khomavolha@users.noreply.github.com>
2020-03-22 16:43:20 +0300

    feat: solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,22 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+      if (!Array.isArray(arr)) throw new Error()
+
+      let resume = [];
+
+      for (let i = 0; i < arr.length; i++) {
+          if (arr[i] == "--discard-next") {
+              i++;
+          } else if (arr[i] == "--discard-prev") {
+              resume.pop();
+          } else if (arr[i] == "--double-next" && i + 1 != arr.length) {
+              resume.push(arr[i + 1]);
+          } else if (arr[i] == "--double-prev" && i - 1 > 0) {
+              resume.push(arr[i - 1]);
+          } else if (arr[i] != "--discard-prev" && arr[i] != "--double-prev" && arr[i] != "--double-next" && arr[i] != "--discard-next") {
+              resume.push(arr[i]);
+          }
+      }
+      return resume;
+
+  };
